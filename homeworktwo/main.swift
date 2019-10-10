1d1a62139c906f6d1a75347d4316d550b067be2c
Nikita Boiko <genertyniki@gmail.com>
2019-10-10 20:45:58 +0300

    finished task one and task two

--- homeworktwo/main.swift
+++ homeworktwo/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+func evenNum(number: Int, devider: Int) -> Bool {
+    var isEven = false
+    if number % devider == 0 {
+        isEven = true
+    }
+    return isEven
+}
+
+print("Введите число:")
+var currentNum: Int = Int(readLine() ?? "0") ?? 0
+print("Число \(currentNum) \(evenNum(number: currentNum, devider: 2) ? "четное" : "нечетное") ")
+
+print("Введите число:")
+currentNum = Int(readLine() ?? "0") ?? 0
+print("Число \(currentNum) \(evenNum(number: currentNum, devider: 3) ? "" : "не") делится без остатка на 3")
 
