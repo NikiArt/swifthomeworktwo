688839e6c543a4eb816150c8cf5fb7199f9a3a74
Nikita Boiko <genertyniki@gmail.com>
2019-10-10 20:31:08 +0300

    Initial Commit

---
+++ homeworktwo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  homeworktwo
+//
+//  Created by Nikita Boiko on 10/10/2019.
+//  Copyright © 2019 Nikita Boiko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
